6bf8ea90868ca1ba98b78b421e996d60050bd422
Gonzalo Hernán Perez <gonzaloperez1987@gmail.com>
2022-04-20 20:33:09 -0300

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,1 +1,12 @@
-document.write("Archivo externo!")
+var nombreUsuario = "Juan"//escribo var para arrancar a definir, espacio y el nombre de la variable identificando cada palabra con una mayúscula para facilitar su lectura
+var largo = 8
+var ancho = 10
+var superficie = largo * ancho
+document.write("Nombre del usuario: " + nombreUsuario)
+document.write(superficie)
+superficie = superficie + 1
+document.writeln("usando superficie = superficie + 1 queda" + superficie)
+superficie += 1
+document.write("usando superficie += 1 queda" + superficie)
+superficie++
+document.write("usando superficie++ queda" + superficie)
