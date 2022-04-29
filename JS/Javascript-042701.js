2e410c2785e448012d59fd7b22533c6ecdd9952a
Gonzalo Hernán Perez <gonzaloperez1987@gmail.com>
2022-04-28 21:42:36 -0300

    Actualización
    Modificaciones generales, se eliminaron pero casi se volvió a foja cero

---
+++ JS/Javascript-042701.js
@@ -0,0 +1,9 @@
+console.log("hola mundo")
+document.write("Escribo sobre el documento desde el archivo .js")
+function multiplicar( numero1, numero2){
+    var resultado = numero1 * numero2
+    document.write("el resultado es ",
+    resultado,"<br>")
+}
+var volumenCaja = (x, y, z) => x * y * z
+var saludar = (nombre) => "Hola "+nombre
